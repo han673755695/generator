c0908e0942a9202e36aa34d41bb0a62f0ab0dc77
hanyidong <han673755695@163.com>
2020-04-11 17:05:11 +0800

    init

---
+++ src/main/java/com/yuanhang/generator/mapper/PmsSkuStockMapper.java
@@ -0,0 +1,7 @@
+package com.yuanhang.generator.mapper;
+
+import com.yuanhang.generator.entry.PmsSkuStock;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PmsSkuStockMapper extends Mapper<PmsSkuStock> {
+}
